598d4e78c21e46c9bdd831bd1515568e19d28664
Minhpn <phanngocminh168@gmail.com>
2025-04-08 22:07:28 +0700

    fix ui

--- app/src/main/java/com/example/taskmanager/screen/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/example/taskmanager/screen/notifications/NotificationsViewModel.java
@@ -10,7 +10,7 @@ public class NotificationsViewModel extends ViewModel {
 
     public NotificationsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("Tính năng đang được phát triển");
     }
 
     public LiveData<String> getText() {
